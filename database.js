0b364a30bbea63abe4af704cd101aa7c9255ae22
Márcio DEV <31147955+DMVMarcio@users.noreply.github.com>
2018-03-30 18:55:36 -0300

    Add files via upload

--- database.js
+++ database.js
@@ -7,3 +7,35 @@ mongoose.connect(process.env.database, {
     console.log("Conectado ao BANCO DE DADOS!");
 })
 
+var Guild = new Schema({
+    _id: {
+        type: String
+    },
+    welcome: {
+        type: Boolean,
+        default: false
+    },
+    welcomechannel: {
+        type: String,
+        default: "Nenhum"
+    },
+    welcomemsg: {
+        type: String,
+        default: "Nenhuma"
+    },
+    byebye: {
+        type: Boolean,
+        default: false
+    },
+    byebyechannel: {
+        type: String,
+        default: "Nenhum"
+    },
+    byebyemsg: {
+        type: String,
+        default: "Nenhuma"
+    }
+})
+
+var Guilds = mongoose.model("Guilds", Guild);
+exports.Guilds = Guilds

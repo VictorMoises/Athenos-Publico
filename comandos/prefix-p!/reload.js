85add534305e34e40065d9adec86dd530bcc9e6e
Márcio DEV <31147955+DMVMarcio@users.noreply.github.com>
2018-03-31 23:19:14 -0300

    Update reload.js

--- comandos/prefix-p!/reload.js
+++ comandos/prefix-p!/reload.js
@@ -2,14 +2,13 @@ exports.run = (client, message, args) => {
 
         let reason = args.slice(0).join(' ');
     
-        if (message.author.id === "315263840268976128" || message.author.id === '254042074712768512'|| message.author.id === '425797924232888340'|| message.author.id === '404966710077292544' || message.author.id === '286144811680137218'|| message.author.id === '273691083425447936'){
+        var desenvolvedores = ["315263840268976128", "254042074712768512", "425797924232888340", "404966710077292544", "286144811680137218", "273691083425447936"]
+
+        if(desenvolvedores.includes(message.author.id)) return message.reply("**Sem permissão. :confused:**");
         if (reason.length < 1) return message.reply('**Diga o comando que devo reiniciar!**');
     
         delete require.cache[require.resolve(`./${args[0]}.js`)];
     
         message.channel.sendMessage("**:gear: " + message.author + " Comando " + args[0] + " reiniciado!**");
     
-    } else {
-        message.reply("**Sem permissão. :confused:**");
-    }
 }

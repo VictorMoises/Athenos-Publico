25692ca8a363f8c9940d4e5378749ee0f9218a90
Márcio DEV <31147955+DMVMarcio@users.noreply.github.com>
2018-03-31 23:20:47 -0300

    Update reload.js

--- comandos/prefix-p!/reload.js
+++ comandos/prefix-p!/reload.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     
         var desenvolvedores = ["315263840268976128", "254042074712768512", "425797924232888340", "404966710077292544", "286144811680137218", "273691083425447936"]
 
-        if(desenvolvedores.includes(message.author.id)) return message.reply("**Sem permissão. :confused:**");
+        if(!desenvolvedores.includes(message.author.id)) return message.reply("**Sem permissão. :confused:**");
         if (reason.length < 1) return message.reply('**Diga o comando que devo reiniciar!**');
     
         delete require.cache[require.resolve(`./${args[0]}.js`)];
